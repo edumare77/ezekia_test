498d18dd19f1157b3851b9e2a95ef1c4085a4cbc
5and3Eduardo <5and3eduardo@gmail.com>
2021-02-12 16:38:24 +0000

    Controllers

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Candidate;
+use App\Http\Traits\ImportCsv;
 
 class CandidateController extends Controller
 {
-    //
+
+    // Using trait to import CSV files into DB
+    use ImportCsv;
+
+    /* Method uses trait to populate candidate table 
+    and return the candidates and their jobs
+    */
+
+    public function populateCandidates($fileName) {
+
+        $tableName = app(Candidate::class)->getTable();
+
+        if (!is_null($tableName) ||  $tableName !== '') {
+
+            return $this->importCsvInfile($fileName, $tableName);
+            
+
+        } else {
+
+            return false;
+
+        }
+
+        
+    }
+
+    /* Method return the candidates and their jobs
+    */
+
+    public function getCandidateJobs() {
+
+        $candidatesJobs = Candidate::with('getJobs')->get()->toArray();
+
+        return $candidatesJobs;
+
+    }
+
 }
--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Job;
+use App\Http\Traits\ImportCsv;
 
 class JobController extends Controller
 {
-    //
+
+    // Using trait to import CSV files into DB
+    use ImportCsv;
+
+    /* Method uses trait to populate job table 
+    */
+
+    public function populateJobs($fileName) {
+
+        $tableName = app(Job::class)->getTable();
+
+        if(!is_null($tableName) ||  $tableName !== '') {
+
+            return $this->importCsvInfile($fileName, $tableName);
+
+        } else {
+
+            return false;
+
+        }
+
+
+       
+
+    }
 }
